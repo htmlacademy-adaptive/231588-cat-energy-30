93b2bb9ee47f9fd6b381dad6c31d364b7a3ac77f
Александра <chamomile@MacBook-Pro-Aleksandra.local>
2024-01-11 15:57:57 +0300

    Add marks

---
+++ source/scripts/required.js
@@ -0,0 +1,8 @@
+let submitBtn = document.querySelector('.form__bottom-btn');
+
+submitBtn.addEventListener('click', function() {
+  let inputs = document.querySelectorAll('input[required]');
+  for(let i=0;i<inputs.length;i++){
+    inputs[i].classList.add('form-group__item-input--error');
+  }
+});
