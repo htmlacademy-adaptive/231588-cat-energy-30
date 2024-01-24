bed3cdedd96b0af5adcc701b55efcb59cdf8aaa7
Александра <chamomile@MacBook-Pro-Aleksandra.local>
2024-01-24 23:06:46 +0300

    Add some marks

--- source/scripts/hamburger.js
+++ source/scripts/hamburger.js
@@ -1,19 +1,26 @@
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-header__toggle');
+const navMain = document.querySelector(".main-nav");
+const navToggle = document.querySelector(".toggle");
 
-navMain.classList.remove('main-nav--nojs');
-navToggle.classList.remove('toggle--nojs');
+navMain.classList.remove("main-nav--nojs");
+navToggle.classList.remove("toggle--nojs");
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    navToggle.classList.remove('toggle--open');
-    navToggle.classList.add('toggle--close');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    navToggle.classList.add('toggle--open');
-    navToggle.classList.remove('toggle--close');
-  }
+navToggle.addEventListener('click', () => {
+  navToggle.classList.toggle("toggle--open");
+  navToggle.classList.toggle("toggle--close");
+  navMain.classList.toggle("main-nav--closed");
+  navMain.classList.toggle("main-nav--opened");
 });
+
+// navToggle.addEventListener('click', () => {
+//   if (navMain.classList.contains("main-nav--closed")) {
+//     navMain.classList.remove("main-nav--closed");
+//     navMain.classList.add("main-nav--opened");
+//     navToggle.classList.remove("toggle--open");
+//     navToggle.classList.add("toggle--close");
+//   } else {
+//     navMain.classList.add("main-nav--closed");
+//     navMain.classList.remove("main-nav--opened");
+//     navToggle.classList.add("toggle--open");
+//     navToggle.classList.remove("toggle--close");
+//   }
+// });
